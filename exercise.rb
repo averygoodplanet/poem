0c5642533d2769fdb8f832e316b33e4be2d57d8f
averygoodplanet <averygoodplanet@gmail.com>
2014-04-07 13:28:10 -0500

    puts poem

--- exercise.rb
+++ exercise.rb
@@ -47,19 +47,18 @@ puts "Let's practice everything."
 
 puts "You'd need to know 'bout escapes with \\ that do \n newlines and \t tabs."
 
-# poem = <<POEM
-# \tThe lovely world
-# with logic so firmly planted
-# cannot discern \n the needs of love
-# nor comprehend passion from intuition
-# and requires an explantion
-# \n\t\twhere there is none.
-# POEM
+poem = <<POEM
+The lovely world
+with logic so firmly planted
+cannot discern the needs of love
+nor comprehend passion from intuition
+and requires an explantion
+where there is none.
+POEM
 
-
-# puts "--------------"
-# puts poem
-# puts "--------------"
+puts "--------------"
+puts poem
+puts "--------------"
 
 # five = 10 - 2 + 3 - 5
 # puts "This should be five: %s" % five
